2ee1e0ed89d3f275eba2c25293c2c98aa4f55242
Nillfllheim <nillfllheim@gmail.com>
2022-04-27 18:11:10 -0500

    AprendiendoJS_Arreglos

--- 07.objetos/js/12_app.js
+++ 07.objetos/js/12_app.js
@@ -7,5 +7,18 @@ const producto = {
     disponible: true,
 }
 
-//Object Constructor
+//Object Constructor (es mas dinamico)
 
+function Producto(nombre, precio){
+    this.nombre = nombre
+    this.precio = precio
+    this.disponible = true
+}
+
+const producto2 = new Producto('Monitor de 24"', 650)
+
+console.log(producto2)
+
+const producto3 = new Producto('tablet CHUI 13"', 300)
+
+console.log(producto3)
---
+++ 07.objetos/js/13_app.js
@@ -0,0 +1,11 @@
+const producto = {
+    nombre: 'Monitor de 19"',
+    precio: 300,
+    disponible: true,
+}
+
+console.log(Object.keys(producto)) //'keys' retorna en arreglo
+
+console.log(Object.values(producto)) //'values' retorna los valores
+
+console.log(Object.entries(producto)) // 'entries' retorna todo el objeto
